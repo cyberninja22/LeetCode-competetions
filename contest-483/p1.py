4c3ab49ae1054abc0bbc434fecac3509825eb0a1
cyberninja22 <sharathga@gmail.com>
2026-01-10 20:27:31 -0500

    setting up the files

---
+++ contest-483/p1.py
@@ -0,0 +1,14 @@
+import sys
+import math
+import bisect
+import heapq
+from collections import Counter, defaultdict, deque
+from functools import lru_cache # For memoization (DP)
+
+# Increase recursion depth just in case
+sys.setrecursionlimit(200000)
+
+class Solution:
+    def solve(self, args):
+        # Your code here
+        pass
